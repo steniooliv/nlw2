d0c8fd55c846788342a3973f8b6217b82f58cecc
Stenio Oliveira <steniooliv@gmail.com>
2020-08-09 19:31:38 -0300

    add data from backend

--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -1,17 +1,127 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, ScrollView, Text, TextInput, AsyncStorage } from 'react-native';
+import { BorderlessButton, RectButton } from 'react-native-gesture-handler';
+import { Feather } from '@expo/vector-icons';
+import { useAsyncStorage } from '@react-native-community/async-storage';
 
 import PageHeader from '../../components/PageHeader';
-import TeacherItem from '../../components/TeacherItem';
+import TeacherItem, { Teacher } from '../../components/TeacherItem';
 
+import api from '../../services/api';
 import styles from './styles';
 
 function TeacherList(){
+  const [teachers, setTeachers] = useState([]);
+  const [favorites, setFavorites] = useState<number[]>([]);
+  const [isFilterVisible, setIsFilterVisible] = useState(false);
+
+  const [subject, setSubject] = useState('');
+  const [week_day, setWeekDay] = useState('');
+  const [time, setTime] = useState('');
+
+  useEffect(() => {
+    AsyncStorage.getItem('favorites').then(response => {
+      if (response) {
+        const favoritedTeachers = JSON.parse(response);
+        const favoritedTeachersId = favoritedTeachers.map((teacher: Teacher) => {
+          return teacher.id;
+        })
+
+        setFavorites(favoritedTeachersId);
+      }
+
+    })
+
+  }, [])
+  
+  async function handleFilterSubmit() {
+    const response = await api.get('classes', {
+      params: {
+        subject,
+        week_day,
+        time,
+      }
+    });
+    setIsFilterVisible(false);
+    setTeachers(response.data);
+  }
+
+  function handleToggleFilterVisible(){
+    setIsFilterVisible(!isFilterVisible);
+  }
+
   return (
     <View style={styles.container}>
-      <PageHeader title='Proffys disponíveis' />
+      <PageHeader
+        title='Proffys disponíveis'
+        headerRight={(
+          <BorderlessButton onPress={handleToggleFilterVisible}>
+            <Feather name='filter' size={30} color='#fff' />
+          </BorderlessButton>
+        )}
+      >
+        { isFilterVisible &&(
+          <View style={styles.searchForm}>
+            <Text style={styles.label}>Matéria</Text>
+            <TextInput 
+              style={styles.input}
+              value={subject}
+              onChangeText={text => setSubject(text)}
+              placeholder='Qual a matéria?'
+              placeholderTextColor='#c1bccc'
+            />
 
-      <TeacherItem />
+            <View style={styles.inputGroup}>
+              <View style={styles.inputBlock}>
+                <Text style={styles.label}>Dia da semana</Text>
+                <TextInput 
+                  style={styles.input}
+                  value={week_day}
+                  onChangeText={text => setWeekDay(text)}
+                  placeholder='Qual o dia?'
+                  placeholderTextColor='#c1bccc'
+                />
+              </View>
+
+              <View style={styles.inputBlock}>
+                <Text style={styles.label}>Horário</Text>
+                <TextInput 
+                  style={styles.input}
+                  value={time}
+                  onChangeText={text => setTime(text)}
+                  placeholder='Qual horário'
+                  placeholderTextColor='#c1bccc'
+                />
+              </View>
+            </View>
+            <RectButton onPress={handleFilterSubmit} style={styles.submitButton}>
+              <Text style={styles.submitButtonText}>
+                Filtrar
+              </Text>
+            </RectButton>
+          </View>
+        )}
+
+      </PageHeader>
+
+      <ScrollView
+        style={styles.teacherList}
+        contentContainerStyle={{
+          paddingHorizontal: 16,
+          paddingBottom: 16,
+        }}
+      >
+        {teachers.map((teacher: Teacher) => {
+          return (
+            <TeacherItem
+              key={teacher.id}
+              teacher={teacher}
+              favorited={favorites.includes(teacher.id)}
+            />
+          );
+        })}
+      
+      </ScrollView>
     </View>
   );
 }

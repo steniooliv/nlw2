c2e727d2803f4f8af89d71cd83bc4bc141aff9c0
Stenio Oliveira <steniooliv@gmail.com>
2020-08-09 19:30:24 -0300

    add teacher interface and data from backend

--- mobile/src/components/TeacherItem/index.tsx
+++ mobile/src/components/TeacherItem/index.tsx
@@ -1,25 +1,70 @@
 import React from 'react';
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, Linking } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+
+import hearthOutlineIcon from '../../assets/images/icons/heart-outline.png';
+import unfavoriteIcon from '../../assets/images/icons/unfavorite.png';
+import whatsappIcon from '../../assets/images/icons/whatsapp.png';
 
 import styles from './styles';
 
-function TeacherItem() {
+export interface Teacher {
+  id: number,
+  avatar: string,
+  bio: string,
+  cost: number,
+  name: string,
+  subject: string,
+  whatsapp: string,
+}
+
+interface TeacherItemProps {
+  teacher: Teacher,
+  favorited: boolean,
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function handleLinkToWhatsapp() {
+    Linking.openURL(`whatsapp://send?phone=${teacher.whatsapp}`);
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.profile}>
         <Image 
           style={styles.avatar} 
-          source={{uri:'https://github.com/steniooliv.png'}}
+          source={{ uri: teacher.avatar }}
         />
         <View style={styles.profileInfo}>
-          <Text style={styles.name}>Stenio Oliveira</Text>
-          <Text style={styles.subject}>Matemática</Text>
+          <Text style={styles.name}>{teacher.name}</Text>
+          <Text style={styles.subject}>{teacher.subject}</Text>
         </View>
       </View>
 
       <Text style={styles.bio}>
-        
+        {teacher.bio}
       </Text>
+
+      <View style={styles.footer}>
+        <Text style={styles.price}>
+          Preço/Hora {'   '}
+          <Text style={styles.priceValue}>R$ {teacher.cost}</Text>
+        </Text>
+
+        <View style={styles.buttonsContainer}>
+          <RectButton style={[styles.favoriteButton, styles.favorited]}>
+            {/* <Image source={hearthOutlineIcon} /> */}
+            <Image source={unfavoriteIcon} />
+          </RectButton>
+
+          <RectButton onPress={handleLinkToWhatsapp} style={styles.contactButton}>
+            <Image source={whatsappIcon} />
+            <Text style={styles.contactButtonText}>Entrar em contato</Text>
+          </RectButton>
+
+        </View>
+
+      </View>
     </View>
   );
 }

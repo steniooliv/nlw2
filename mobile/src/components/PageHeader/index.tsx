97e2b72875dbc54f445b2125a9d7ebec51dd470f
Stenio Oliveira <steniooliv@gmail.com>
2020-08-09 19:29:37 -0300

    edit page header component

--- mobile/src/components/PageHeader/index.tsx
+++ mobile/src/components/PageHeader/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 
 import { View, Image, Text } from 'react-native';
 import { BorderlessButton } from 'react-native-gesture-handler';
@@ -11,10 +11,11 @@ import styles from './styles';
 
 interface PageHeaderProps {
   title: string,
+  headerRight?: ReactNode,
 }
 
 
-const PageHeader: React.FC<PageHeaderProps> = ({ title }) => {
+const PageHeader: React.FC<PageHeaderProps> = ({ title, headerRight, children }) => {
   const { navigate } = useNavigation();
   
   function handleGoBack() {
@@ -32,9 +33,15 @@ const PageHeader: React.FC<PageHeaderProps> = ({ title }) => {
         <Image source={logoImg} resizeMode='contain' />
       </View>
 
-      <Text style={styles.title}>
-        { title }
-      </Text>
+      <View style={styles.header}>
+        <Text style={styles.title}>
+          { title }
+        </Text>
+
+        {headerRight}
+      </View>
+
+      {children}
 
     </View>
   );
